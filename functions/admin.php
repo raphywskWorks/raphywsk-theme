a35b9bfedffc452a9304a852ff0ba361871808f0
raphywsk <raphywsk@icloud.com>
2023-07-19 12:31:46 -0300

    Create theme admin settings template

--- functions/admin.php
+++ functions/admin.php
@@ -8,12 +8,43 @@
 
 function rap_add_admin_page() 
 {
+    //Generate Raphywsk admin page
     add_menu_page(
-        'Rap Theme Options', 'Raphywsk', 'manage_options', 'raphywsk-theme', 'rap_create_page', 'dashicons-laptop'
+        'Rap Theme Options', 
+        'Raphywsk', 
+        'manage_options', 
+        'raphywsk_theme', 
+        'rap_create_page', 
+        'dashicons-laptop'
     );
+
+    //Generate Sub Pages
+    add_submenu_page(
+        'raphywsk_theme', 
+        'Rap Theme Options', 
+        'Settings', 
+        'manage_options', 
+        'raphywsk_theme', 
+        'rap_create_page'
+    );
+
+    add_submenu_page(
+        'raphywsk_theme', 
+        'Rap CSS Options', 
+        'Custom CSS', 
+        'manage_options', 
+        'raphywsk_theme_css', 
+        'rap_theme_css_options'
+    );
+
 }
 
 function rap_create_page() 
+{
+    require_once(get_template_directory() . '/templates/raphywsk_settings.php');
+}
+
+function rap_theme_css_options()
 {
 
 }
---
+++ templates/raphywsk_settings.php
@@ -0,0 +1,5 @@
+<h1>Sunset Theme Options</h1>
+<h3 class="title">Manage Settings</h3>
+<p>Customize the default Theme Appearance</p>
+
+<?php bloginfo('name'); ?>
